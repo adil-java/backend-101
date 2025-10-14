e6de8578e679faef8ce9d2ea5647524b78919b86
adil-java <adiljavaid125@gmail.com>
2025-10-14 07:21:55 -0700

    Add cookie-parser and cors middleware, implement ApiError and ApiResponse classes, and create asyncHandler utility

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 import express from 'express';
 
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
-
-
+app.use(express.json({
+    limit: '50kb'
+}));
+app.use(cookieParser());
+app.use(cors({
+origin: process.env.CORS_ORIGIN,
+credentials: true,
+}));
+app.use(express.urlencoded({ extended: true, limit: '50kb' }));
+app.use(express.static('public'));
  
 
 export {app}
