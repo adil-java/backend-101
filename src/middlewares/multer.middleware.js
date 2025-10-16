56033cd93f5d296831db81b268511a3f320dd802
adil-java <adiljavaid125@gmail.com>
2025-10-16 07:49:03 -0700

    Add multer middleware for handling file uploads

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/temp') // specify the destination directory
+    },
+    filename: function (req, file, cb) {
+
+      cb(null, file.originalname) // specify the file name
+    }
+  })
+  
+  const upload = multer({ storage });
+
+  export { upload};
